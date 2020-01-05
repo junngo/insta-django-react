54932c08281642b60e1faae8f1430a5b6af3636d
junngo <myeongjun.ko@gmail.com>
2020-01-05 19:30:27 +0900

    using django import-export package

--- blink/models.py
+++ blink/models.py
@@ -1,11 +1,13 @@
+from django.utils import timezone
 from django.db import models
+
 
 # Create your models here.
 class Sentence(models.Model):
     no = models.IntegerField()
     sentence_ko = models.CharField(max_length=255)
     sentence_eng = models.CharField(max_length=255)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
     source = models.ForeignKey('Source', 
                                 null=True,
                                 on_delete=models.SET_NULL,
--- honey_lang/settings.py
+++ honey_lang/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blink.apps.BlinkConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+IMPORT_EXPORT_USE_TRANSACTIONS = True
