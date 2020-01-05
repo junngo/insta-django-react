a8036bec25bb37cfa4a8bc9144283e5fe166f7c0
junngo <myeongjun.ko@gmail.com>
2020-01-05 21:57:54 +0900

    created blink detail api

--- blink/urls.py
+++ blink/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blink'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/', views.detail, name='detail'),
 ]
--- blink/views.py
+++ blink/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Sentence
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_sentence_list = Sentence.objects.all()
+    context = {'latest_sentence_list': latest_sentence_list}
+    
+    return render(request, 'blink/index.html', context)
+
+def detail(request):
+
+    if request.method == 'POST':
+        print(request.POST['choice'])
+        start_sentence = request.POST['min']
+        end_sentence = request.POST['max']
+
+        latest_sentence_list = Sentence.objects.filter(no__range=(start_sentence, end_sentence))
+        context = {'latest_sentence_list': latest_sentence_list}
+
+        return render(request, 'blink/detail.html', context)
+
+    elif request.method == 'get':
+        pass
