d2ca0893f6a27d8d9f84a9683e7aa957928e8000
junngo <myeongjun.ko@gmail.com>
2020-01-30 17:19:03 +0900

    added NODE_PATH at .env file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-import store from "./redux/configureStore";
-import './index.css';
+import App from 'App';
+import store from "redux/configureStore";
+import 'index.css';
 import { Provider } from 'react-redux';
 
 
