54f3ce46d184aeb9147c1dd833045b9fd512c97b
junngo <myeongjun.ko@gmail.com>
2020-02-04 16:04:29 +0900

    setting jwt auth env - djangorestframework-jwt package

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
-
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -14,6 +14,7 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
+    path('api-token-auth/', obtain_jwt_token),
     path("users/", include("honey_lang.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
