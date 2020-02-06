a3852d26f53b78d5a3dc3731eef593634b135623
junngo <myeongjun.ko@gmail.com>
2020-02-06 16:52:22 +0900

    [FE] creating facebook login redux action

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import SignupForm from "./presenter";
 
 
@@ -8,6 +9,10 @@ class Container extends Component {
         fullname: "",
         username: "",
         password: "",
+    };
+
+    static propTypes = {
+        facebookLogin: PropTypes.func.isRequired
     };
 
     render() {
@@ -40,7 +45,8 @@ class Container extends Component {
     };
 
     _handleFacebookLogin = response => {
-        console.log(response);
+        const { facebookLogin } = this.props;
+        facebookLogin(response.accessToken);
     };
 }
 
