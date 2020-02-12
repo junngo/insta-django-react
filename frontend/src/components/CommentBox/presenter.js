4a4b033578fd44298ac23e270e9203b71b93485f
junngo <myeongjun.ko@gmail.com>
2020-02-12 10:18:55 +0900

    [FE] added react-textarea-autosize package for comment box

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
+import Textarea from "react-textarea-autosize";
 
 const CommentBox = (props, context) => (
     <form>
-        <textarea placeholder={context.t("Add a comment...")} />
+        <Textarea placeholder={context.t("Add a comment...")} />
     </form>
 );
 
