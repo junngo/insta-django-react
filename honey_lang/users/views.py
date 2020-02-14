8dde91acbfcf9f54ff1adc2c5c958c1c581ba45b
junngo <myeongjun.ko@gmail.com>
2020-02-14 10:11:03 +0900

    [FE] working follow and unfollow

--- honey_lang/users/views.py
+++ honey_lang/users/views.py
@@ -4,7 +4,10 @@ from django.urls import reverse
 from django.views.generic import DetailView, RedirectView, UpdateView
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
